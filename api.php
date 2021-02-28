d7af1567d8ea380b7a2a11122256a380baae950d
Rinshan Kolayil <rinshank@asjqatar.com>
2021-02-28 09:32:23 +0300

    first commit

---
+++ api.php
@@ -0,0 +1,17 @@
+set_include_path(get_include_path() . PATH_SEPARATOR . 'RSA');
+include_once 'RSA/Crypt/RSA.php';
+$rsa = new Crypt_RSA();
+$rsa->setPassword('password');
+$rsa->loadKey(file_get_contents('RSA/private.key'));
+$rsa->setPassword();
+$rsa->getPrivateKey();
+$OAuthToken = $_POST['OAuthToken'];
+$signature = $rsa->sign($plaintext);
+$publickey = file_get_contents('RSA/public.pem');
+$clent_id = $_POST['client_id'];
+$redirect_uri = $_POST['redirect_uri'];
+$$public_key_posted = $_POST['public_key_posted'];
+$publickey = '-----BEGIN PUBLIC KEY-----' . "\r\n" . $public_key_posted . "\r\n" . "-----END PUBLIC KEY-----";
+$rsa->loadKey($publickey);
+$is_verified =  $rsa->verify($OAuthToken, $signature) ? 'verified' : 'unverified';
+echo $is_verified;
